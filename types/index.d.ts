7b30cfbbe66af1bf9a8e2f69e526635332ef5464
Mortada <rida19974@gmail.com>
2024-09-29 10:48:41 +0300

    CHORE include new types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,5 @@
+import { Timestamp } from "firebase/firestore"; // Import Firestore Timestamp
+
 declare interface FriendRequest {
   id: string;
   fromUserId: string;
@@ -7,11 +9,29 @@ declare interface FriendRequest {
   createdAt: { seconds: number; nanoseconds: number };
 }
 
-declare interface Friend {
-  createdAt: { seconds: number; nanoseconds: number }; // Assuming Firebase's Timestamp format
+// declare interface Friend {
+//   createdAt: { seconds: number; nanoseconds: number }; // Assuming Firebase's Timestamp format
+//   displayName: string;
+//   email: string;
+//   id: string;
+//   photoURL: string;
+//   username: string;
+// }
+
+// Type for lastMessage object
+declare interface LastMessage {
+  senderId: string;
+  text: string;
+  timestamp: Timestamp;
+}
+
+// Type for the main object
+declare interface UserData {
+  createdAt: Timestamp | Date; // Depending on how you retrieve it from Firestore, it could be a Timestamp or Date
   displayName: string;
   email: string;
   id: string;
+  lastMessage: LastMessage;
   photoURL: string;
   username: string;
 }
